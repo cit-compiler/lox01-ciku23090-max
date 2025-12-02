a7c1e88e25de4ac40f6c9937ed1efbf21d5de2c5
ciku23090 <ciku23090@g.nihon-u.ac.jp>
2025-12-02 10:28:27 +0900

    add string

--- codes/com/craftinginterpreters/lox/Scanner.java
+++ codes/com/craftinginterpreters/lox/Scanner.java
@@ -17,7 +17,6 @@ class Scanner {
   Scanner(String source) {
     this.source = source;
   }
-  // 【修正】ここに あった `}` を削除しました
 
   List<Token> scanTokens() {
     while (!isAtEnd()) {
@@ -43,6 +42,40 @@ class Scanner {
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
       case '*': addToken(STAR); break; 
+
+      case '!':
+        // 【修正1】ddToken -> addToken に直しました
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+      case '/':
+        if (match('/')) {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+        break;
+
+      case ' ':
+      case '\r':
+      case '\t':
+        // Ignore whitespace.
+        break;
+
+      case '\n':
+        line++;
+        break;
+      case '"': string(); break;
+
       default:
         Lox.error(line, "Unexpected character.");
         break;
@@ -65,4 +98,38 @@ class Scanner {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
   }
-} // 【修正】クラスの閉じカッコ `}` はファイルの最後に移動しました
+
+  // 【修正2】ここにあった `}` を削除し、メソッドをクラスの中に入れました
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
+  }
+
+  private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
+  }
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
+  }
+} // 【修正2】クラスの閉じカッコ `}` を一番最後に移動しました
